3acf1c686b402bb3ad50dd623061b3ea17622c81
mnel <epos.smurfmarco@gmail.com>
2024-04-13 14:51:08 +0100

    Adding initial code for generating requests

---
+++ src/main/kotlin/nel/marco/Main.kt
@@ -0,0 +1,54 @@
+package nel.marco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+open class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+}
+
+
+data class AccessRequest(
+    val accessType: AccessType,
+    val targetUri: String,
+    val credentialToAdd: String,
+    val reason:String
+)
+
+enum class AccessType {
+    READ,
+    WRITE,
+    ADMIN
+}
+
+interface RequestPermission {
+
+    fun generateRequest(accessRequest: AccessRequest)
+    fun sendRequest(accessRequest: AccessRequest)
+    fun approveRequest(accessRequest: AccessRequest)
+    fun declineRequest(accessRequest: AccessRequest)
+}
+
+class RequestAccessUserEndpoint : RequestPermission{
+    override fun generateRequest(accessRequest: AccessRequest) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendRequest(accessRequest: AccessRequest) {
+        TODO("Not yet implemented")
+    }
+
+    override fun approveRequest(accessRequest: AccessRequest) {
+        TODO("Not yet implemented")
+    }
+
+    override fun declineRequest(accessRequest: AccessRequest) {
+        TODO("Not yet implemented")
+    }
+
+
+}
